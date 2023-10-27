d436e027ac54dbf120c1b82f42939f01bf269355
Ara Pulido <ara.pulido@datadoghq.com>
2023-10-27 12:41:16 +0200

    Reuse code for sync and async

--- webapp/index.py
+++ webapp/index.py
@@ -1,11 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import threading
 
 app = Flask(__name__)
-
-def async_call():
-    requests.get('http://async:5002/load?nthreads=2&cycles=10000000')
 
 @app.route('/')
 def hello_world():
@@ -14,12 +11,24 @@ def hello_world():
 @app.route("/do_request")
 def do_work():
 
-    requests.get('http://sync:5001/load?nthreads=2&cycles=10000000')
+    nthreads_sync = int(request.args.get('nthreads_sync', 2))
+    nthreads_async = int(request.args.get('nthreads_async', 2))
+    cycles_sync = int(request.args.get('cycles_sync', 5000000))
+    cycles_async = int(request.args.get('cycles_async', 5000000))
+    call_type = request.args.get('type', 'both')
+
+    payload = {'nthreads': nthreads_sync, 'cycles': cycles_sync}
+
+    if call_type in ('both', 'sync'):
+        requests.get('http://sync:5001/load', params=payload)
 
     print("Finished sync calls")
 
-    t = threading.Thread(target=async_call)
-    t.daemon = True
-    t.start()
+    payload = {'nthreads': nthreads_async, 'cycles': cycles_async}
+
+    if type in ('both', 'async'):
+        t = threading.Thread(target=async_call, args=(payload,))
+        t.daemon = True
+        t.start()
 
     return 'Finished calls!', 200
